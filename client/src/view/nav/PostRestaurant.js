48bfc4db5339387806b4e3f07f2e7aada8d4149e
dongjin <dongqui@stuintas.com>
2019-04-11 18:59:14 +0900

    select place in search

--- client/src/view/nav/PostRestaurant.js
+++ client/src/view/nav/PostRestaurant.js
@@ -1,5 +1,5 @@
 import { Input } from "react-materialize";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Rating from 'react-rating';
 import './PostRestaurant.css';
 import SearchListItem from './SearchListItem';
@@ -7,22 +7,27 @@ import emptyStar from "../reviews/img/star-empty.png";
 import fullStart from "../reviews/img/star-full.png";
 import useInput from "../../hooks/useInput";
 
-const PostRestaurant = () => {
+const PostRestaurant = (props) => {
 
     const { close } = props;
     const [ rate, setRate ] = useState(0);
     const [ placesData, setPlacesData ] = useState([]);
     const [ selectedPlace, setSelectedPlace] = useState(null);
     const [ reviewContent, setReviewContent ] = useInput('');
+    const searchInput = useRef(null);
     const places = new window.daum.maps.services.Places();
 
     const searchReataurantWithname = (event) => {
         const restaurantName = event.target.value;
+        if (!restaurantName.length) {
+            setPlacesData([]);
+            return;
+        }
         places.keywordSearch(restaurantName, searchRestaurantCallback, {
             location: new window.daum.maps.LatLng(37.545486, 127.051632),
             radius: 3000,
             size: 5,
-        } );
+        });
     };
     const searchRestaurantCallback = (placeData, status) => {
         if (status === window.daum.maps.services.Status.OK) {
@@ -33,16 +38,31 @@ const PostRestaurant = () => {
     const clickPlaceList = (place) => () => {
         setSelectedPlace(place);
         setPlacesData([]);
+        searchInput.current.value = place.place_name;
+    };
+
+    const submitNewPost = () => {
+        const { id, place_name, y, x, place_url } = selectedPlace;
+        const data = {
+            daumPId: id,
+            placeName: place_name,
+            map: {latitude: y, longitude: x},
+            placeUrl: place_url,
+            review: reviewContent,
+            rating: rate
+            // user._id, filter
+        }
+
+        //request post
     };
 
     return (
         <div>
             {console.log('render!!')}
             음식점
-            <input value={selectedPlace ? selectedPlace.place_name : ''} onChange = {searchReataurantWithname} placeholder = '음식점 이름을 적어주세요!' />
-            { selectedPlace}
+            <input ref={searchInput} onChange={searchReataurantWithname} placeholder = '음식점 이름을 적어주세요!' />
             <ul>
-                { placesData.map( data => <SearchListItem clickPlaceList={clickPlaceList} data={data} setSelectedPlace={setSelectedPlace} />)}
+                { placesData.map( data => <SearchListItem key={data.id} clickPlaceList={clickPlaceList} data={data} />)}
             </ul>
 
             <Input name='group1' type='radio' value='korean' label='한식' className='with-gap' />
@@ -60,9 +80,8 @@ const PostRestaurant = () => {
             <textarea
                 rows='7'
                 cols='15'
-                label='review'
                 style={{width:'100%', height:'15%'}}
-                onChange={ setReviewContent }
+                onChange={ (e) => setReviewContent(e.target.value) }
             />
             <button>POST</button>
             <button onClick={close}>CLOSE</button>
--- client/src/view/nav/SearchListItem.js
+++ client/src/view/nav/SearchListItem.js
@@ -5,7 +5,7 @@ const SearchListItem = (props) => {
     const { place_name, address_name } = props.data;
 
     return (
-      <li className="place-search-list" onClick={() => props.setSelectedPlace(props.data)}>
+      <li className="place-search-list" onClick={props.clickPlaceList(props.data)}>
         <h5>{place_name}</h5>
         <span>{address_name}</span>
       </li>

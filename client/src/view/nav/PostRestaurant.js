00f25764bee991050a11381d0956ad23bb300559
dongjin <dongqui@stuintas.com>
2019-04-12 14:43:15 +0900

    radio button in post component

--- client/src/view/nav/PostRestaurant.js
+++ client/src/view/nav/PostRestaurant.js
@@ -1,4 +1,4 @@
-import { Input } from "react-materialize";
+import { RadioGroup } from "react-materialize";
 import React, { useState, useRef } from "react";
 import Rating from 'react-rating';
 import './PostRestaurant.css';
@@ -9,84 +9,93 @@ import useInput from "../../hooks/useInput";
 
 const PostRestaurant = (props) => {
 
-    const { close } = props;
-    const [ rate, setRate ] = useState(0);
-    const [ placesData, setPlacesData ] = useState([]);
-    const [ selectedPlace, setSelectedPlace] = useState(null);
-    const [ reviewContent, setReviewContent ] = useInput('');
-    const searchInput = useRef(null);
-    const places = new window.daum.maps.services.Places();
+  const { close } = props;
+  const [ rate, setRate ] = useState(0);
+  const [ placesData, setPlacesData ] = useState([]);
+  const [ selectedPlace, setSelectedPlace] = useState(null);
+  const [ reviewContent, setReviewContent ] = useInput('');
+  const [ filter, setFilter ] = useState(null);
+  const searchInput = useRef(null);
+  const places = new window.daum.maps.services.Places();
 
-    const searchReataurantWithname = (event) => {
-        const restaurantName = event.target.value;
-        if (!restaurantName.length) {
-            setPlacesData([]);
-            return;
-        }
-        places.keywordSearch(restaurantName, searchRestaurantCallback, {
-            location: new window.daum.maps.LatLng(37.545486, 127.051632),
-            radius: 3000,
-            size: 5,
-        });
-    };
-    const searchRestaurantCallback = (placeData, status) => {
-        if (status === window.daum.maps.services.Status.OK) {
-            setPlacesData(placeData);
-        }
-    };
+  const searchReataurantWithname = (event) => {
+    const restaurantName = event.target.value;
+    if (!restaurantName.length) {
+      setPlacesData([]);
+      return;
+    }
+    places.keywordSearch(restaurantName, searchRestaurantCallback, {
+      location: new window.daum.maps.LatLng(37.545486, 127.051632),
+      radius: 3000,
+      size: 5,
+    });
+  };
+  const searchRestaurantCallback = (placeData, status) => {
+    if (status === window.daum.maps.services.Status.OK) {
+      setPlacesData(placeData);
+    }
+  };
 
-    const clickPlaceList = (place) => () => {
-        setSelectedPlace(place);
-        setPlacesData([]);
-        searchInput.current.value = place.place_name;
-    };
+  const clickPlaceList = (place) => () => {
+    setSelectedPlace(place);
+    setPlacesData([]);
+    searchInput.current.value = place.place_name;
+  };
 
-    const submitNewPost = () => {
-        const { id, place_name, y, x, place_url } = selectedPlace;
-        const data = {
-            daumPId: id,
-            placeName: place_name,
-            map: {latitude: y, longitude: x},
-            placeUrl: place_url,
-            review: reviewContent,
-            rating: rate
-            // user._id, filter
-        }
+  const submitNewPost = () => {
+    const { id, place_name, y, x, place_url } = selectedPlace;
+    const data = {
+      daumPId: id,
+      placeName: place_name,
+      map: {latitude: y, longitude: x},
+      placeUrl: place_url,
+      review: reviewContent,
+      rating: rate,
+      filter
+    }
 
-        //request post
-    };
+    //request post
+  };
+  console.log(filter);
+  return (
+    <div>
+      <h4 id="post-header">새 맛집 등록</h4>
+      <input id="post-input" ref={searchInput} onChange={searchReataurantWithname} placeholder = '음식점 이름을 적어주세요!' />
+      <ul>
+        { placesData.map( data => <SearchListItem key={data.id} clickPlaceList={clickPlaceList} data={data} />)}
+      </ul>
+      <div id="radio-container">
+        <RadioGroup
+          name="filter"
+          radioClassNames="post_radio"
+          options={[
+            {label: '한식', value: 'korean'},
+            {label: '양식', value: 'western'},
+            {label: '일식', value: 'japanese'},
+            {label: '중식', value: 'chinese'},
+            {label: '일식', value: 'japanese'},
+            {label: 'etc', value: '기타'}
+          ]}
+        />
+      </div>
 
-    return (
-        <div>
-            {console.log('render!!')}
-            음식점
-            <input ref={searchInput} onChange={searchReataurantWithname} placeholder = '음식점 이름을 적어주세요!' />
-            <ul>
-                { placesData.map( data => <SearchListItem key={data.id} clickPlaceList={clickPlaceList} data={data} />)}
-            </ul>
 
-            <Input name='group1' type='radio' value='korean' label='한식' className='with-gap' />
-            <Input name='group1' type='radio' value='western' label='양식' className='with-gap' />
-            <Input name='group1' type='radio' value='japanese' label='일식' className='with-gap' />
-            <Input name='group1' type='radio' value='chinese' label='중식' className='with-gap' />
-            <Input name='group1' type='radio' value='etc' label='기타' className='with-gap' />
-
-            <Rating
-                emptySymbol = {<img alt='' src={emptyStar} className="icon" />}
-                fullSymbol = {<img alt='' src={fullStart} className="icon" />}
-                onChange = { rate => setRate(rate) }
-            />
-            음식 맛을 평가해주세요!
-            <textarea
-                rows='7'
-                cols='15'
-                style={{width:'100%', height:'15%'}}
-                onChange={ (e) => setReviewContent(e.target.value) }
-            />
-            <button>POST</button>
-            <button onClick={close}>CLOSE</button>
-        </div>
-    )
+      <Rating
+        emptySymbol = {<img alt='' src={emptyStar} className="icon" />}
+        fullSymbol = {<img alt='' src={fullStart} className="icon" />}
+        onChange = { rate => setRate(rate) }
+      />
+      <textarea
+        rows='7'
+        cols='15'
+        id="post-textarea"
+        onChange={ (e) => setReviewContent(e.target.value) }
+        placeholder='어땠어요?'
+      />
+      <button className="post-btn">POST</button>
+      <button className="post-btn" onClick={close}>CLOSE</button>
+    </div>
+  )
 };
 
 export default PostRestaurant;
